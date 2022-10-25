522ede5eec79c3becb5a819d832b268e58cfc5d2
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-10-25 19:57:01 +0530

    slider+page_config+posters updated

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -25,7 +25,7 @@ st.markdown(hide_streamlit_style2, unsafe_allow_html=True)
 page_bg_img = '''
 <style>
 .stApp {
-background-image: url("https://wallpaperaccess.com/full/869905.gif");
+background-image: url("https://i.ibb.co/t869zV7/istockphoto-1124347647-170667a.jpg");
 background-size: cover;
 }
 </style>
