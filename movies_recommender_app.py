5f763b181804ee0770e18d69249018b1e20b59a3
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-11-30 17:07:38 +0530

    Update movies_recommender_app.py

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -6,6 +6,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
+from streamlit_extras.no_default_selectbox import selectbox
 import tmdbsimple as tmdb
 
 
@@ -78,7 +79,7 @@ try:
         recommendation.append(int(i))
 except:
     st.write('Oops! Seems like this movie is not listed on our Database. You can also try checking out the official name of the movie from Google and try again!')
-    selected_movie=st.selectbox('You can also choose a movie from the list below', match_names,format_func=lambda x: 'Select an option' if x == '' else x)
+    selected_movie=selectbox('You can also choose a movie from the list below', match_names,format_func=lambda x: 'Select an option' if x == '' else x)
     movie_index_=map_names(selected_movie)
     for i in data['recommendaions'][movie_index_].split(','):
         recommendation.append(int(i))
