aed15d20fc3f96735d89ea583cf95c69ad98100b
Ayan <59755186+ayanatherate@users.noreply.github.com>
2022-11-30 17:10:10 +0530

    Update movies_recommender_app.py

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -79,7 +79,7 @@ try:
         recommendation.append(int(i))
 except:
     st.write('Oops! Seems like this movie is not listed on our Database. You can also try checking out the official name of the movie from Google and try again!')
-    selected_movie=selectbox('You can also choose a movie from the list below', match_names,format_func=lambda x: 'Select an option' if x == '' else x)
+    selected_movie=selectbox('You can also choose a movie from the list below', match_names)
     movie_index_=map_names(selected_movie)
     for i in data['recommendaions'][movie_index_].split(','):
         recommendation.append(int(i))
